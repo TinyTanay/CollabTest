b592c7a95af17e568515ba5f38e9655943b705e4
GodSri86 <sriram.sundaresan@education.nsw.gov.au>
2021-11-18 17:29:20 +1100

    Added practise on Variables & Data Types

--- sriram_python_practise_area.py
+++ sriram_python_practise_area.py
@@ -6,7 +6,16 @@ name = "Sriram"
 print(f'My name is {name}')
 
 #This is the place where I will do my Python practise
+#Drawing Shapes
 print("   /|")
 print("  / |")
 print(" /  |")
 print("/___|")
+#Variables & Data Types
+character_name = "Sriram"
+character_age = "13"
+print("There once was a boy named " + character_name + ". ")
+print("He was " + character_age + " years old. ")
+character_name = "Tanay"
+print("He liked the name " + character_name + ". ")
+print("And he was fine with being " + character_age + " years old. ")
