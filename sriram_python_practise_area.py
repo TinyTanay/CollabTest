976c3a6c967ce370431601c5d325e57834ecbb0f
GodSri86 <sriram.sundaresan@education.nsw.gov.au>
2021-11-18 17:33:01 +1100

    Added Working With Strings

--- sriram_python_practise_area.py
+++ sriram_python_practise_area.py
@@ -6,12 +6,17 @@ name = "Sriram"
 print(f'My name is {name}')
 
 #This is the place where I will do my Python practise
+
+
 #Drawing Shapes
+
 print("   /|")
 print("  / |")
 print(" /  |")
 print("/___|")
+
 #Variables & Data Types
+
 character_name = "Sriram"
 character_age = "13"
 print("There once was a boy named " + character_name + ". ")
@@ -19,3 +24,5 @@ print("He was " + character_age + " years old. ")
 character_name = "Tanay"
 print("He liked the name " + character_name + ". ")
 print("And he was fine with being " + character_age + " years old. ")
+
+#Working With Strings
