12b39e2aff0c50d85de3bb0f02c3b24ab2bbc721
tanay.kulshreshtha <tanay.kuls@gmail.com>
2021-11-19 13:21:45 +1100

    Created tanay directory and added parent class pet and child class cat.

--- Tanay/tanay.py
+++ Tanay/tanay.py
@@ -1,4 +1,28 @@
-def my_name():
-    return("My name is Tanay")
+class Pet():
+    def __init__(self, age, name)
+    self.age = age
+    self.name = name
 
-print(my_name())
+class Cat(Pet):
+    def __init__(self, age, name, colour, breed):
+        super().__init__(age, name)
+        self.colour = colour
+        self.breed = breed
+
+    @staticmethod
+    def speak():
+        print("MEOW")
+
+    @classmethod
+    def show():
+        print(f"I am a {self.breed} and {self.age} years old")
+
+class Dog():
+    pass
+
+p = Pet(36, "Simmons")
+
+c = Cat("Green", "Tabby")
+
+c.speak()
+c.show()
--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-print("Hello World")
-
-print("This is an addition")
-
-class Pet:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-name = input("What is your name?")
-print(name)
+print("This is a joint file")
