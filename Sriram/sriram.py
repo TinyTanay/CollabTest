46db59c838db9ba51eaea6bfab7f0aac7311110b
GodSri86 <sriram.sundaresan@education.nsw.gov.au>
2021-11-23 21:04:30 +1100

    Added more useful functions while working with numbers

--- Sriram/sriram.py
+++ Sriram/sriram.py
@@ -74,3 +74,19 @@ my_num = 5
 print(abs(my_num))
 #This does the power of
 print(pow(5, 23))
+#This finds the bigger Numbers
+print(max(5, 10, 21))
+#This finds the smaller Numbers
+print(small(5, 10, 21))
+#This will round Numbers
+print(round(5.6))
+print(round(5.2))
+#In order to add more functions simply typer
+from math import *
+#Then there are some more functions.
+#To find the floor function
+print(floor(3.2))
+#To find the ceiling function.
+print(ceil(3.2))
+#This is one final useful function to square root a Numbers
+print(sqrt(36))
