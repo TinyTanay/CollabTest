f2ed90a6b4fffde9b3e28550f33e537aa5baa699
GodSri86 <sriram.sundaresan@education.nsw.gov.au>
2021-11-25 18:37:30 +1100

    Checking an error

--- Sriram/calculators.py
+++ Sriram/calculators.py
@@ -10,7 +10,7 @@ print("This is a Calculator that will do most things but only with 2 numbers")
 num1 = input("Enter a number: ")
 num2 = input("Enter another number: ")
 symbol = input("Enter the symbol for what you want to do: ")
-if symbol = "+":
+if symbol is "+":
     print(float(num1) + float(num2))
 elif symbol = "-":
     print(float(num1) - float(num2))
