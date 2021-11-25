d2a82386297a66b9de4fc01c30e06b35e6d8622a
GodSri86 <sriram.sundaresan@education.nsw.gov.au>
2021-11-25 18:38:08 +1100

    Fixed up some errors

--- Sriram/calculators.py
+++ Sriram/calculators.py
@@ -12,9 +12,9 @@ num2 = input("Enter another number: ")
 symbol = input("Enter the symbol for what you want to do: ")
 if symbol is "+":
     print(float(num1) + float(num2))
-elif symbol = "-":
+elif symbol is "-":
     print(float(num1) - float(num2))
-elif symbol = "x":
+elif symbol is "x":
     print(float(num1) x float(num2))
 else:
-    print(float(num1)  / float(num2))
+    print(float(num1) / float(num2))
