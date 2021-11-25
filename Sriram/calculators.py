361379d4806041fb55ef2437a058a25755c44f1d
GodSri86 <sriram.sundaresan@education.nsw.gov.au>
2021-11-25 18:36:47 +1100

    Attempted to make a fairly advanced calculator

--- Sriram/calculators.py
+++ Sriram/calculators.py
@@ -4,3 +4,17 @@ num1 = input("Enter a number: ")
 num2 = input("Enter another number: ")
 answer = float(num1) + float(num2)
 print(answer)
+
+#Building an Advanced Calculator
+print("This is a Calculator that will do most things but only with 2 numbers")
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+symbol = input("Enter the symbol for what you want to do: ")
+if symbol = "+":
+    print(float(num1) + float(num2))
+elif symbol = "-":
+    print(float(num1) - float(num2))
+elif symbol = "x":
+    print(float(num1) x float(num2))
+else:
+    print(float(num1)  / float(num2))
