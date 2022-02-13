813875728a66675878c7eb401a4b3a4e8ca85492
GodSri86 <sriram.sundaresan@education.nsw.gov.au>
2022-02-13 17:21:38 +1100

    Added some code

---
+++ Sriram/list_functions.py
@@ -0,0 +1,5 @@
+lucky_numbers
+friends = ["Kevin", "Karen", "Jim", "Oscar", "Toby"]
+#We can use the extend function to add the lucky_numbers list to the end of the friends list by doing this
+friends.extend(lucky_numbers)
+print(friends)
