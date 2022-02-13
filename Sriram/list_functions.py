28c34aad921cc6bd85b41b4c4de4db3248ffb0ac
GodSri86 <sriram.sundaresan@education.nsw.gov.au>
2022-02-13 17:22:29 +1100

    Fixed a minute error

--- Sriram/list_functions.py
+++ Sriram/list_functions.py
@@ -1,4 +1,4 @@
-lucky_numbers
+lucky_numbers = [4, 6, 12, 34, 63, 3]
 friends = ["Kevin", "Karen", "Jim", "Oscar", "Toby"]
 #We can use the extend function to add the lucky_numbers list to the end of the friends list by doing this
 friends.extend(lucky_numbers)
