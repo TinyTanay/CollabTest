67eb55f74896e18a052ed0f80042632e5958f274
GodSri86 <sriram.sundaresan@education.nsw.gov.au>
2022-02-13 16:28:25 +1100

    Added a basic List

---
+++ Sriram/Lists.py
@@ -0,0 +1,11 @@
+#Lists are basically like variables, but unlike them, it can store multiple pieces of data.
+
+sports = ["cricket", "soccer", "basketball"]
+
+#Each of those has an index.
+#Cricket being 0, Soccer being 1 and Basketball being 2
+#If you want to print a specific one, you can do this
+
+print(sports[2])
+
+#Which would print Basketball
