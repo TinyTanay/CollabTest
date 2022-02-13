893362370831997c92ffc9016ec0c594fb9704e6
GodSri86 <sriram.sundaresan@education.nsw.gov.au>
2022-02-13 20:05:39 +1100

    Added some lines to the mad libs game

--- Sriram/mad_libs_game.py
+++ Sriram/mad_libs_game.py
@@ -1,8 +1,11 @@
 
 person = input("Enter the name of someone you know: ")
-characteristic = input("Enter a characteristic: ")
+adjective = input("Enter an adjective: ")
 item = input("Enter an item: ")
+food = input("Enter a food item: ")
 
-print(person + " is smart")
-print("There is no question about their " + characteristic)
+print(person + " is weiord")
+print("There is no question about that")
 print("If you were to get a(n) " + item + " and hit them, they would rage")
+print("Screen time is " + adjective)
+print("But if we eat " + food " we will surely work out the password")
