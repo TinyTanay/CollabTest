b7c69d85412444b5214fa6adb426ad68e9b29691
GodSri86 <sriram.sundaresan@education.nsw.gov.au>
2022-02-13 12:48:49 +1100

    saved

---
+++ Sriram/mlg2.py
@@ -0,0 +1,8 @@
+
+person = input("Enter the name of someone you know: ")
+characteristic = imput("Enter a characteristic: ")
+item = input("Enter an item: ")
+
+print(person + " is smart")
+print("There is no question about their " + characteristic)
+print("If you were to get a(n) " + item + " and hit them, they would rage")
