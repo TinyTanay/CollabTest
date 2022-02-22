6ce8350cd92500e22461bae5c05b5c8cadca2105
GodSri86 <sriram.sundaresan@education.nsw.gov.au>
2022-02-22 19:38:26 +1100

    added simple function

---
+++ Sriram/functions.py
@@ -0,0 +1,9 @@
+#Here is a simple function:
+
+def sayhi():
+    print("Hello User")
+
+#Always remember to put the colon after defining or creating a function
+#If I left it like this nothing would happen. To call it back however, I can do so
+
+sayhi()
