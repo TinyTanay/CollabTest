25cfafed153d829566bb9ee097345bd86e6eaccd
GodSri86 <sriram.sundaresan@education.nsw.gov.au>
2022-02-23 18:05:13 +1100

    added some more functions

--- Sriram/functions.py
+++ Sriram/functions.py
@@ -6,4 +6,11 @@ def sayhi():
 #Always remember to put the colon after defining or creating a function
 #If I left it like this nothing would happen. To call it back however, I can do so
 
+print("Top")
 sayhi()
+print("Bottom")
+
+#If you want you can add code in between the function and when you call it back like so
+
+#You can also add code after the function is called back
+#The above will print, Top, the function which is hello user and then bottom
