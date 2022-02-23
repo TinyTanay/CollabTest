86c6417bba85918df43149e2ccc8e7f7f69fa9fc
GodSri86 <sriram.sundaresan@education.nsw.gov.au>
2022-02-23 18:48:02 +1100

    Added return statement for function

--- Sriram/return_statement.py
+++ Sriram/return_statement.py
@@ -1,3 +1,17 @@
 #The return allows us to return info from a function
 
-print("Hello")
+def cube(num):
+    return num*num*num
+
+print(cube(4))
+
+#Whcih will print 4^3 which is 64, however without the return function, it would print none.
+#You can also do this
+
+def square(num):
+    return num*num*num
+
+result = square(4)
+square(result)
+
+#Which will rturn 64
